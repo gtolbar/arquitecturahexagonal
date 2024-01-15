b4bb036cd1c16c9129bc6b563904c8af2119edee
gtolbar <toledo970501@gmail.com>
2024-01-15 15:20:23 -0500

    Se crea proyecto

---
+++ src/test/java/com/example/pokedex_to_hexagonal/PokedexToHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokedex_to_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexToHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
