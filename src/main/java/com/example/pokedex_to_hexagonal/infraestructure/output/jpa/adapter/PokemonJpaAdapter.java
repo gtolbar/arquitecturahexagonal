54c536d94a1e10e8ed8734b5351393d4777e0b8e
gtolbar <toledo970501@gmail.com>
2024-02-13 23:13:55 -0500

    Se crea adaptador pokemon jpa

--- src/main/java/com/example/pokedex_to_hexagonal/infraestructure/output/jpa/adapter/PokemonJpaAdapter.java
+++ src/main/java/com/example/pokedex_to_hexagonal/infraestructure/output/jpa/adapter/PokemonJpaAdapter.java
@@ -2,7 +2,11 @@ package com.example.pokedex_to_hexagonal.infraestructure.output.jpa.adapter;
 
 import com.example.pokedex_to_hexagonal.domain.model.Pokemon;
 import com.example.pokedex_to_hexagonal.domain.spi.IPokemonPersistencePort;
+import com.example.pokedex_to_hexagonal.infraestructure.exception.NoDataFoundException;
 import com.example.pokedex_to_hexagonal.infraestructure.exception.PokemonAlreadyExistsException;
+import com.example.pokedex_to_hexagonal.infraestructure.exception.PokemonNotFoundException;
+import com.example.pokedex_to_hexagonal.infraestructure.output.jpa.entity.PokemonEntity;
+import com.example.pokedex_to_hexagonal.infraestructure.output.jpa.mapper.PokemonEntityMapper;
 import com.example.pokedex_to_hexagonal.infraestructure.output.jpa.repository.IPokemonRespository;
 import lombok.RequiredArgsConstructor;
 
@@ -13,32 +17,38 @@ public class PokemonJpaAdapter implements IPokemonPersistencePort {
 
     private final IPokemonRespository pokemonRespository;
 
-    private final PokemonJpaAdapter pokemonJpaAdapter;
+    private final PokemonEntityMapper pokemonEntityMapper;
 
     @Override
     public void savePokemon(Pokemon pokemon) {
         if(pokemonRespository.findByNumber(pokemon.getNumber()).isPresent()){
             throw new PokemonAlreadyExistsException();
         }
+        pokemonRespository.save(pokemonEntityMapper.toEntity(pokemon));
     }
 
     @Override
     public List<Pokemon> getAllPokemon() {
-        return null;
+        List<PokemonEntity> pokemonEntityList = pokemonRespository.findAll();
+        if(pokemonEntityList.isEmpty()){
+            throw new NoDataFoundException();
+        }
+        return pokemonEntityMapper.toPokemonList(pokemonEntityList);
     }
 
     @Override
     public Pokemon getPokemon(Long pokemonNumber) {
-        return null;
+        return pokemonEntityMapper.toPokemon(pokemonRespository.findByNumber(pokemonNumber)
+                .orElseThrow(PokemonNotFoundException::new));
     }
 
     @Override
     public void updatePokemon(Pokemon pokemon) {
-
+        pokemonRespository.save(pokemonEntityMapper.toEntity(pokemon));
     }
 
     @Override
     public void deletePokemon(Long pokemon) {
-
+        pokemonRespository.deleteByNumber(pokemon);
     }
 }
